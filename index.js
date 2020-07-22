b28a24397e874b409585cdc0f48dfc3da17c3730
M-Espinosa <mati.9209@hotmail.com>
2020-07-21 21:45:06 -0300

    Nuevo Cambio

---
+++ index.js
@@ -0,0 +1,27 @@
+const coolImages = require('cool-images');
+const moment = require('moment');
+const fs = require('fs');
+// coolImages.one(); // 'https://unsplash.it/300/500?image=125'
+
+let arrayUrlImages = coolImages.many();
+console.log(arrayUrlImages)
+
+arrayUrlImages.forEach(element => {
+    console.log(element)
+});
+
+
+//log.txt
+fs.readFile('log.txt', 'utf-8', (err, datos) => {
+    console.log(datos);
+})
+fs.appendFile('log.txt', fechaHora + '\n' + arrayUrlImages + '\n', (error) => {
+    if (error) throw Error;
+    console.log('exito')
+})
+
+
+//moment
+//fecha y hora
+let fechaHora = moment().format('YYYY/MM/DD HH:mm:ss')
+console.log(fechaHora)
